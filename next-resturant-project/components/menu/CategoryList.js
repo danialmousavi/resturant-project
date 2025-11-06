b3bfac83e31140efbbe02c9c6399651960e06b95
danialmousavi <danialmoosavi69@gmail.com>
2025-11-06 12:55:05 +0330

    handle filter category menu

--- next-resturant-project/components/menu/CategoryList.js
+++ next-resturant-project/components/menu/CategoryList.js
@@ -1,14 +1,35 @@
-import React from "react";
+"use client"
 
-export default function CategoryList({ categories }) {
-  return (
-    <div className="filter-list">
-      <div className="form-label">دسته بندی</div>
-      <ul>
-        {categories.map((category) => (
-          <li className="my-2 cursor-pointer filter-list-active">{category.name}</li>
-        ))}
-      </ul>
-    </div>
-  );
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+
+export default function CategoriesList({ categories }) {
+    const pathname = usePathname();
+    const router = useRouter();
+    const searchParams = useSearchParams()
+
+    function handleClick(id) {
+        const params = new URLSearchParams(searchParams);
+        params.set('category', id);
+        params.delete('page');
+
+        router.replace(`${pathname}?${params.toString()}`)
+    }
+
+    return (
+        <div className="filter-list">
+            <div className="form-label">
+                دسته بندی
+            </div>
+            <ul>
+                {categories.map(category => (
+                    <li key={category.id}
+                        onClick={() => handleClick(category.id)}
+                        className={searchParams.has('category') && searchParams.get('category') == category.id ? "my-2 filter-list-active" : "my-2 cursor-pointer"}
+                    >
+                        {category.name}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    )
 }
