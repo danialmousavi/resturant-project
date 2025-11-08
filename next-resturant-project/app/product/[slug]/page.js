ef549015e8a54496eabb7deb1c3a5cba59b97c83
danialmousavi <danialmoosavi69@gmail.com>
2025-11-08 23:03:20 +0330

    fix bug

--- next-resturant-project/app/product/[slug]/page.js
+++ next-resturant-project/app/product/[slug]/page.js
@@ -39,7 +39,8 @@ export default async function ProductPage({ params }) {
                   <div className="mt-5 d-flex">
                     <button className="btn-add">افزودن به سبد خرید</button>
                     <div className="input-counter ms-4">
-                      <span className="plus-btn">+</span>
+                      <span className="plus-btn">+</span> 
+                      
                       <div className="input-number">1</div>
                       <span className="minus-btn">-</span>
                     </div>
