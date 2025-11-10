3da98f29bf275a45d135c3e34e85cfa57dc5c3e6
danialmousavi <danialmoosavi69@gmail.com>
2025-11-10 22:01:47 +0330

    login code

--- next-resturant-project/components/modules/LoginForm/Form.js
+++ next-resturant-project/components/modules/LoginForm/Form.js
@@ -4,6 +4,8 @@ import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import styles from "@/styles/login.module.css";
 import { LoginSchema } from "@/utils/Schema/LoginScema";
+import LoginAction from "@/utils/actions/Auth";
+import { toast } from "react-toastify";
 
 
 export default function LoginForm() {
@@ -11,42 +13,71 @@ export default function LoginForm() {
 
   const handleSubmit = async (values, { resetForm }) => {
     setLoading(true);
-    try {
-      // شبیه‌سازی درخواست به سرور
-      await new Promise((resolve) => setTimeout(resolve, 1500));
-
-      alert(`شماره ${values.phone} ارسال شد ✅`);
-      resetForm();
-    } catch (err) {
-      console.error("Error:", err);
-    } finally {
-      setLoading(false);
+    console.log(values);
+    const result=await LoginAction(values)
+    console.log(result);
+    if(result.success){
+            toast.success("کد با موفقیت ارسال شد", {
+              position: "bottom-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: false,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });      
+            setLoading(false)
+            resetForm()
+    }else{
+            toast.error("متاسفیم مشکلی پیش آمده است", {
+              position: "bottom-right",
+              autoClose: 2000,
+              hideProgressBar: false,
+              closeOnClick: false,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "colored",
+            });      
+            resetForm()
     }
+    // try {
+    //   // شبیه‌سازی درخواست به سرور
+    //   await new Promise((resolve) => setTimeout(resolve, 1500));
+
+    //   alert(`شماره ${values.phone} ارسال شد ✅`);
+    //   resetForm();
+    // } catch (err) {
+    //   console.error("Error:", err);
+    // } finally {
+    //   setLoading(false);
+    // }
   };
 
   return (
     <Formik
-      initialValues={{ phone: "" }}
+      initialValues={{ cellphone: "" }}
       validationSchema={LoginSchema}
       onSubmit={handleSubmit}
     >
       {({ handleSubmit }) => (
         <Form onSubmit={handleSubmit}>
           <div className="mb-3 text-start">
-            <label htmlFor="phone" className="form-label">
+            <label htmlFor="cellphone" className="form-label">
               شماره موبایل
             </label>
 
             <Field
               type="tel"
-              name="phone"
+              name="cellphone"
               placeholder="مثلاً 09123456789"
               className="form-control text-center"
               dir="ltr"
             />
 
             <ErrorMessage
-              name="phone"
+              name="cellphone"
               component="div"
               className={styles.errorText}
             />
--- next-resturant-project/utils/actions/Auth.js
+++ next-resturant-project/utils/actions/Auth.js
@@ -1,17 +1,29 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 export default async function LoginAction(values) {
   try {
     const res = await fetch("http://localhost:8000/api/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
+        "accept":"application/json"
       },
       body: JSON.stringify(values),
     });
     console.log("resssssssssss", res);
-    if (res.status == 201) {
-      return {
+    if (res.status == 200) {
+        const data=await res.json();
+        console.log("dataaaaaaaaaaa",data);
+        cookies().set({
+          name:"loginToken",
+          value:data.data.login_token,
+          httpOnly:true,
+          path:"/"
+        })
+              return {
+        data,
         success: true,
       };
     } else {
