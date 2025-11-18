37cbe3e9daac08f0948ee04b8e726ca83f2d8e47
danialmousavi <danialmoosavi69@gmail.com>
2025-11-18 17:34:49 +0330

    create cart page

--- next-resturant-project/utils/redux/slices/cartSlice.js
+++ next-resturant-project/utils/redux/slices/cartSlice.js
@@ -3,22 +3,30 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState={
     cart:[]
 }
-export const cartSlice=createSlice({
-    name:"Cart",
+export const cartSilce = createSlice({
+    name: 'cart',
     initialState,
-    reducers:{
-        addToCart:(state,action)=>{
-            // console.log(state,action);
-            const {product,qty}=action.payload
-            state.cart=[...state.cart,{...product,qty:qty}]
+    reducers: {
+        addToCart: (state, action) => {
+            const { product, qty } = action.payload;
+            state.cart = [...state.cart, { ...product, qty: qty }]
+
             console.log(state.cart);
-            
         },
-        removeFromCart:(state,action)=>{
-            console.log(action);
-            state.cart=state.cart.filter(p=>p.id!==action.payload)
+        removeFromCart: (state, action) => {
+            state.cart = state.cart.filter(p => p.id !== action.payload)
+        },
+        increment: (state, action) => {
+            state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty + 1 } : p)
+        },
+        decrement: (state, action) => {
+            state.cart = state.cart.map(p => p.id === action.payload ? { ...p, qty: p.qty - 1 } : p)
+        },
+        clearCart: (state) => {
+            state.cart = [];
         }
     }
 })
-export const {addToCart,removeFromCart}=cartSlice.actions
-export const cartReducer=cartSlice.reducer
+
+export const { addToCart, removeFromCart, increment, decrement, clearCart } = cartSilce.actions;
+export const cartReducer = cartSilce.reducer;
